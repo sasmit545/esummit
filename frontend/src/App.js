9126049853fc421e9f8407e39ccefa8d9f70d3a6
sasmit <sasmitshashwat@gmail.com>
2025-03-16 23:18:32 +0530

    routing fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './navbar/Navbar';
 // import Hero from './component/hero_section/Hero';
 import Team from './team';
+import EventPage from './EventPage';
 
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<h1>Here is Hero section </h1>} />
-        <Route path="/events" element={<h1>Events Page</h1>} />
+        <Route path="/events" element={<EventPage />} />
         <Route path="/speakers" element={<h1>Speakers Page</h1>} />
         <Route path="/team" element={<Team />} />
       </Routes>
