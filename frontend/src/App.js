143a3231c48f14400ccae1f15a89a0d77aa6d4b7
Enum2 <sujalbs2018@gmail.com>
2025-03-16 23:51:17 +0530

    events.js added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./navbar/Navbar";
 // import Hero from './component/hero_section/Hero';
-import Team from './team';
-
+import Team from "./team";
+import EventPage from "./EventPage";
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<h1>Here is Hero section </h1>} />
-        <Route path="/events" element={<h1>Events Page</h1>} />
+        <Route path="/events" element={<EventPage />} />
         <Route path="/speakers" element={<h1>Speakers Page</h1>} />
         <Route path="/team" element={<Team />} />
       </Routes>
