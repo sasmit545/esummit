d93904bb3ef56e6c8d539ccffcb5b2b0f4ff4bca
Klaidoscope14 <saagarchaitanya80@gmail.com>
2025-03-17 00:01:16 +0530

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './navbar/Navbar';
 import ESummitPage from './HomePage';
+import InvestorsPage from './InvestorsPage';
+import SponsorsPage from './SponsorsPage';
 // import Hero from './component/hero_section/Hero';
 import Team from './team';
 import EventPage from './EventPage';
@@ -10,10 +12,9 @@ const App = () => {
   return (
     <Router>
       <Navbar />
-      <ESummitPage/>
       <Routes>
-        <Route path="/" element={<h1>Here is Hero section </h1>} />
-        <Route path="/events" element={<EventPage />} />
+        <Route path="/" element={<ESummitPage />} />
+        <Route path="/events" element={<h1>Events Page</h1>} />
         <Route path="/speakers" element={<h1>Speakers Page</h1>} />
         <Route path="/team" element={<Team />} />
       </Routes>
