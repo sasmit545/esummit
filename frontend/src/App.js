11d02adc9b7bea464a5253660f03e7f123d2e9ed
sasmit <sasmitshashwat@gmail.com>
2025-03-16 21:28:46 +0530

    team page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './navbar/Navbar';
 // import Hero from './component/hero_section/Hero';
+import Team from './team';
 
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
         <Route path="/" element={<h1>Here is Hero section </h1>} />
         <Route path="/events" element={<h1>Events Page</h1>} />
         <Route path="/speakers" element={<h1>Speakers Page</h1>} />
-        <Route path="/team" element={<h1>Team Page</h1>} />
+        <Route path="/team" element={<Team />} />
       </Routes>
     </Router>
   );
