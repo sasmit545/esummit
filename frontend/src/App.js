208d245c2b9557cdb5d1c58745e5472eeb35bf89
sasmit <sasmitshashwat@gmail.com>
2025-03-17 19:37:03 +0530

    added data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import Navbar from "./navbar/Navbar";
 // import Hero from './component/hero_section/Hero';
 import Team from "./team";
 import EventPage from "./EventPage";
-
+import ESummitPage from './HomePage';
+import About from "./About/about";
+import SpeakersPage from "./speakers";
 const App = () => {
   return (
     <Router>
@@ -17,7 +19,7 @@ const App = () => {
         <Route path="/" element={<ESummitPage/>} />
         <Route path="/events" element={<About/>} />
 
-        <Route path="/speakers" element={<h1>Speakers Page</h1>} />
+        <Route path="/speakers" element={<SpeakersPage />} />
         <Route path="/team" element={<Team />} />
       </Routes>
     </Router>
