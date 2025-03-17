5867eadfe4096d14416863de643e6a7084873671
Klaidoscope14 <saagarchaitanya80@gmail.com>
2025-03-17 16:30:00 +0530

    Made Changes in Home Page

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ESummitPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-[#0A1929] overflow-hidden relative text-white text-center font-sans">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-[#0A1929] overflow-hidden relative text-white text-center font-sans">
       {/* Background Image */}
-      <div className="absolute w-full h-full">
+      <div className="absolute inset-0">
         <img
           src="/background.png"
           alt="Background"
@@ -12,29 +12,29 @@ const ESummitPage = () => {
         />
       </div>
 
-      <div className="relative z-10 max-w-7xl mx-auto px-4 py-12">
+      <div className="relative z-10 max-w-7xl w-full mx-auto px-4 py-12">
         
         {/* E-Summit Logo Section */}
-        <div className="relative flex justify-center items-center mb-8">
+        <div className="relative flex flex-col items-center mb-8">
           <img
             src="/es2.svg"
             alt="E-Summit Logo"
-            className="w-[1333.63px] h-96"
+            className="w-[80%] max-w-[900px] h-auto"
           />
           
           {/* Bulb Image */}
           <img
             src="/bulb.svg"
             alt="Bulb"
-            className="w-[600px] h-[550px] left-[280px] top-[40px] absolute mix-blend-overlay opacity-50"
+            className="absolute top-1/2 left-1/2 w-[50%] max-w-[400px] h-auto transform -translate-x-1/2 -translate-y-1/2 mix-blend-overlay opacity-50"
           />
           
           {/* Date */}
-          <div className="absolute right-8 top-[90%] transform -translate-y-1/2 text-right">
-          <div className="w-44 h-28 text-center justify-center"><span class="text-white text-3xl font-bold font-['Poppins'] capitalize">21st-23rd<br/> </span>
-          <span class="text-sky-400 text-3xl font-bold font-['Poppins'] capitalize">March</span></div>
-            {/* <p className="text-xl md:text-2xl font-bold">21st - 23rd</p>
-            <p className="text-xl md:text-2xl font-bold text-[#2BB8FF]">March</p> */}
+          <div className="absolute bottom-4 right-4 md:right-8 md:top-[90%] text-right">
+            <div className="w-44 h-28 text-center">
+              <span className="text-white text-3xl font-bold capitalize">21st-23rd<br/></span>
+              <span className="text-sky-400 text-3xl font-bold capitalize">March</span>
+            </div>
           </div>
         </div>
         
